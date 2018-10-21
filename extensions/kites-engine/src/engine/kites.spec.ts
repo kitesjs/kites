05095dea3c6b9df17911f10c3795f598daccb948
Vunb <nhubaovu@gmail.com>
2018-10-21 14:17:30 +0700

    update bdd test

--- extensions/kites-engine/src/engine/kites.spec.ts
+++ extensions/kites-engine/src/engine/kites.spec.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 import * as path from 'path';
 import {KitesCore} from './kites';
 
@@ -67,7 +67,7 @@ describe('kites engine', () => {
 
 describe('kites logs', () => {
     it('should not log to console by default', async () => {
-        var core = new KitesCore({
+        let core = new KitesCore({
             discover: false
         });
 
@@ -82,5 +82,119 @@ describe('kites logs', () => {
             let stdoutContent = stdMocks.flush();
             expect(stdoutContent.stdout.length).eq(0, 'stdout is empty');
         }).init();
+    });
+
+    it('should keep silent logs', async () => {
+        let core = new KitesCore({
+            discover: false,
+            logger: {
+                silent: true
+            }
+        });
+
+        stdMocks.use({
+            print: true
+        });
+
+        let kites = await core.init();
+        stdMocks.restore();
+        let stdoutContent = stdMocks.flush();
+        expect(stdoutContent.stdout.length).eq(0, 'stdout is empty');
+
+        let allTransportAreSilent = Object.keys(kites.logger.transports).every((name) => kites.logger.transports[name].silent === true);
+        expect(allTransportAreSilent).eq(true, 'all transports are silent');
+    });
+
+    it('should have Debug transport for logs enabled by default', () => {
+        new KitesCore({
+            discover: false
+        }).init().then((kites) => {
+            expect(kites.logger.transports).to.have.property('debug');
+        });
+    });
+
+    it('should fail to configure custom transport that does not have enough options', async () => {
+        let core = new KitesCore({
+            discover: false,
+            logger: {
+                console: {
+                    transport: 'console'
+                }
+            }
+        });
+
+        await core.init().catch((err) => {
+            expect(err).is.instanceOf(Error);
+            assert.match(err.message, /option "level" is not specified or has an incorrect value/);
+        });
+    });
+
+    it('should not load disabled transports for loggger', async () => {
+        let core = new KitesCore({
+            discover: false,
+            logger: {
+                console: {
+                    level: 'debug',
+                    transport: 'console'
+                },
+                memory: {
+                    enabled: false,
+                    level: 'debug',
+                    transport: 'memory',
+                }
+            }
+        });
+
+        let kites = await core.init();
+        assert.isUndefined(kites.logger.transports.memory, 'memory transport shoud be undefined');
+        assert.isNotNull(kites.logger.transports.debug, 'debug transport should be not undefined or null');
+    });
+
+    it('should configure custom transports for logger', async () => {
+        let core = new KitesCore({
+            discover: false,
+            logger: {
+                console: {
+                    level: 'debug',
+                    transport: 'console'
+                },
+                memory: {
+                    level: 'debug',
+                    transport: 'memory'
+                }
+            }
+        });
+
+        let kites = await core.init();
+        assert.isNotNull(kites.logger.transports.memory, 'memory transport shoud be not undefined or null');
+        assert.isNotNull(kites.logger.transports.debug, 'debug transport should be not undefined or null');
+    });
+});
+
+describe('kites initializeListeners', () => {
+    it('should fire initialize listeners on custom extension', async () => {
+        let core = new KitesCore({
+            discover: false,
+            logger: {
+                console: {
+                    level: 'debug',
+                    transport: 'console'
+                }
+            }
+        });
+
+        core.use({
+            directory: '',
+            main: (kites, definition) => {
+                kites.initializeListeners.add('big gun', () => {
+                    kites.logger.debug('Big gun fires!!!');
+                    kites.customExtensionInitialized  = true;
+                });
+            },
+            name: 'test',
+        });
+
+        let newkites = await core.init();
+        expect(newkites.customExtensionInitialized).eq(true);
     });
 });
