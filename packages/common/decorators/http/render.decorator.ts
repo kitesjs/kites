db1a141cb7d4ac681724e30303eacb0367b3a5f3
Vunb <nhubaovu@gmail.com>
2019-06-16 17:43:13 +0700

    implement @Route decorator

--- packages/common/decorators/http/render.decorator.ts
+++ packages/common/decorators/http/render.decorator.ts
@@ -1,5 +1,4 @@
 
-import 'reflect-metadata';
 import { RENDER_METADATA } from '../../constants';
 
 /**
---
+++ packages/common/http/request-medthod.ts
@@ -0,0 +1,10 @@
+export enum RequestMethod {
+  GET = 0,
+  POST,
+  PUT,
+  DELETE,
+  PATCH,
+  ALL,
+  OPTIONS,
+  HEAD,
+}
