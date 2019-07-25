f67246fb9fe0df04ed4ce2c66140acbc008c8ad5
Vunb <nhubaovu@gmail.com>
2019-07-25 21:34:02 +0700

    add injector decorate

--- packages/common/decorators/core/injectable.decorator.ts
+++ packages/common/decorators/core/injectable.decorator.ts
@@ -1,4 +1,4 @@
-import { INJECTABLE_METADATA_KEY, SCOPE_OPTIONS_METADATA } from '../../constants';
+import { INJECTABLE_METADATA_KEY, PARAMTYPES_METADATA, SCOPE_OPTIONS_METADATA } from '../../constants';
 import { ScopeOptions } from '../../interfaces/scope-options.interface';
 import { Type } from '../../interfaces/type.interface';
 
@@ -9,13 +9,21 @@ export interface InjectableOptions extends ScopeOptions { }
  * This class can inject dependencies through constructor.
  * Those dependencies have to belong to the same extension.
  */
-export function Injectable(options?: InjectableOptions) {
+export function Injectable() {
   return (target: any) => {
-    Reflect.defineMetadata(INJECTABLE_METADATA_KEY, options || true, target);
+
+    if (Reflect.hasOwnMetadata(INJECTABLE_METADATA_KEY, target)) {
+      throw new Error('Duplicated injectable decorator');
+    }
+
+    const types = Reflect.getMetadata(PARAMTYPES_METADATA, target) || [];
+
+    Reflect.defineMetadata(INJECTABLE_METADATA_KEY, types, target);
     return target;
   };
 }
 
 export function isInjectable<T>(target: Type<T>) {
-  return Reflect.getMetadata(INJECTABLE_METADATA_KEY, target) === true;
+  // return Reflect.getMetadata(INJECTABLE_METADATA_KEY, target) === true;
+  return Reflect.hasOwnMetadata(INJECTABLE_METADATA_KEY, target);
 }
