052cfaa8735601e455aca41eed98ad1d45d78895
Vunb <nhubaovu@gmail.com>
2019-09-18 13:05:55 +0700

    add awaitable once util

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -3,3 +3,4 @@ import 'reflect-metadata';
 export * from './constants';
 export * from './decorators';
 export * from './interfaces';
+export * from './utils';
--- packages/core/engine/kites-factory.spec.ts
+++ packages/core/engine/kites-factory.spec.ts
@@ -1,8 +1,9 @@
+import { once } from '@kites/common';
 import { expect } from 'chai';
 import { engine, KITES_INSTANCE } from './kites-factory';
 import { KitesInstance } from './kites-instance';
 
-describe('kites init', () => {
+describe('kites factory', () => {
   it('should init a new kites instance without options!', async () => {
     const app = await engine().init();
     app.logger.info('A new kites started!');
@@ -13,5 +14,18 @@ describe('kites init', () => {
       const instance = kites.container.inject(KITES_INSTANCE);
       expect(instance).instanceOf(KitesInstance);
     }).init();
+  });
+
+  it('should wait an event', (done) => {
+    engine().use(async (kites: KitesInstance) => {
+      try {
+        const v = await once(kites, 'ready');
+        expect(v).to.be.an('array', 'once event as a promise return an array!');
+        expect(v[0]).instanceOf(KitesInstance);
+        done();
+      } catch (error) {
+        done(error);
+      }
+    }).init().then(() => true).catch(done);
   });
 });
