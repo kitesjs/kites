6f1fb7bd3a66cce14207de6dcebbad0302bd2e9e
Vunb <nhubaovu@gmail.com>
2019-11-28 14:46:38 +0700

    Refactor options for logger

--- packages/core/logger/index.ts
+++ packages/core/logger/index.ts
@@ -9,15 +9,9 @@ import { DebugTransport } from './debug-transport';
  * @param options
  */
 export function getLogger(name: string, options?: any): Logger {
-  // TODO: Refactor options for logger (not for transport)
   if (!loggers.has(name)) {
     const {format, transports} = options;
     const defaultTransports = transports || [];
-
-    if (defaultTransports && defaultTransports.length === 0) {
-      // add default Debug transport
-      defaultTransports.push(new DebugTransport(options, name));
-    }
 
     const loggerOptions: LoggerOptions = defaults(
     {
@@ -51,6 +45,14 @@ export function getLogger(name: string, options?: any): Logger {
         console.error('------------------------');
       }
     });
+  } else {
+    loggers.get(name).clear();
+  }
+
+  const {debug} = options;
+  if (debug !== false) {
+    // add default Debug transport
+    loggers.get(name).add(new DebugTransport(options, name));
   }
 
   return loggers.get(name);
@@ -60,21 +62,7 @@ export function getLogger(name: string, options?: any): Logger {
  * Get or create logger with default `debug` transport
  */
 export function getDebugLogger(name: string, options?: any) {
-  if (!loggers.has(name)) {
-    loggers.add(name, {
-      exitOnError: false,
-      level: 'info',
-      format: logFormat.combine(
-        logFormat.splat(), // formats level.message based on Node's util.format().
-        logFormat.label({ label: name }),
-        logFormat.colorize(),
-        logFormat.timestamp(),
-        logFormat.printf(({ level, message, label, timestamp }) => `${timestamp} [${label}] ${level}: ${message}`)
-      ),
-      transports: [new DebugTransport(options, name)],
-    });
-  }
-  return loggers.get(name);
+  return getLogger(name, {...options, transports: [new DebugTransport(options, name)]});
 }
 
 export {
